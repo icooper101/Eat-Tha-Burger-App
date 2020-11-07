0fd289cda54dd81f21099b3cf7dba7dbce332768
icooper101 <icooperlife@gmail.com>
2020-11-06 19:34:10 -0500

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
+const { POINT_CONVERSION_COMPRESSED } = require("constants");
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
+};
+
+
 
 
 connection.connect(function(err) {
